6641447c9e45708643f8ad6df307f48c1a7e7f11
Timothy Ko <tko@spikes.com>
2017-09-18 23:41:47 -0500

    added Aboutme section

---
+++ frontend/src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+    render() {
+        return(
+            <section className="about-me-section">
+                <div className="container">
+                    <h3>ABOUT ME</h3>
+                    <div className="row">
+
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+export default AboutMe;
