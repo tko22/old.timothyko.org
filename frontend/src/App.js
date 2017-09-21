464fd9957c27c3b3ff0bd0328ac9087c3ca28857
Timothy Ko <tko@spikes.com>
2017-09-21 14:07:23 -0500

    added skills section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Redirect,Route} from 'react-router-dom'
 import Main from './components/Main'
 import Header from "./components/header";
 import 'animate.css/animate.css';
+import ProjectsPage from "./components/ProjectsPage";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
               <Header/>
               <main>
                   <Route exact path="/" component={Main} />
+                  <Route exact path="/projects" component={ProjectsPage}/>
               </main>
           </div>
       );
