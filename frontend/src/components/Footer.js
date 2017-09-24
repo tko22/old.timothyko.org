51e44bc72deb5bc49a6633c7a56054dee299883c
Timothy Ko <tko@spikes.com>
2017-09-24 00:41:57 -0500

    edited links to resume

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
                     <div className="footer-wrapper-right">
                         <span className="footer-text-span"><a href="mailto:tk2@illinois.edu" style={{color:'black'}}>Email</a></span>
                         <span className="grey-bar"> | </span>
-                        <span className="footer-text-span"><a href="mailto:timothy.l.ko@gmail.com" style={{color:'black'}}>Resume</a></span>
+                        <span className="footer-text-span"><a href="/files/resume.pdf" style={{color:'black'}}>Resume</a></span>
                         <span className="grey-bar"> | </span>
                         <span className="footer-text-span"><a href="https://medium.com/@timmykko">Blog</a></span>
                         <span className="grey-bar"> | </span>
