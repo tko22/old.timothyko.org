0ce09a23bc28f33096e93c36befdee1b8206e7cf
Timothy Ko <timothy.l.ko@gmail.com>
2018-01-04 17:23:49 -0800

    added resumepage

--- frontend/src/components/ResumePage.js
+++ frontend/src/components/ResumePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Resume from './Resume'
+import Projects from './Projects'
+import DevSkills from './DevSkills'
 class ResumePage extends Component {
     render() {
         return(
