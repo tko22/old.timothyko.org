631e83f87ea7b38cfeb7773876a458c11654c20f
Timothy Ko <timothy.l.ko@gmail.com>
2018-01-14 11:07:45 -0800

    added twitter

--- frontend/src/components/AboutMe.js
+++ frontend/src/components/AboutMe.js
@@ -19,7 +19,8 @@ class AboutMe extends Component {
                                 <p>I enjoy the process of envisioning solutions and designing architectures of software products
                                     and diving deep into the code and functionalities. I love learning cutting-edge technologies(Blockchain!!) and
                                     I am always looking to work with brilliant people to develop innovate and simple solutions
-                                    while pushing me to the next level.
+                                    while pushing me to the next level. Check out my <a href="https://medium.com/@timmykko">blog</a>, where I write articles and guides
+                                    on topics ranging from Django to deployment processes to Docker to Daaps on Ethereum!
                                     <hr width="40" style={{marginLeft:0}}/>
                                     And oh yeah. I'm a sophomore studying Computer Engineering at the University of Illinois Urbana-Champaign.
                                     When not coding, I love going outdoors to enjoy nature, whether it's
@@ -39,6 +40,7 @@ class AboutMe extends Component {
                             <a href="https://www.linkedin.com/in/timothy-ko/" className="personal-links">LinkedIn</a> 
                             <a href="mailto:tk2@illinois.edu" className="personal-links">Email</a>
                             <a href="/projects" className="personal-links">My Projects</a>
+                            <a href="https://twitter.com/timmykko6" className="personal-links">Twitter</a>
                         </div>
                     </div>
                 </Reveal>
